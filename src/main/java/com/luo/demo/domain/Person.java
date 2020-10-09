16330499675d81fae57388848e665b4b501f9a68
luoxinyu <luoxinyu@wondersgroup.com>
2020-10-09 17:08:31 +0800

    1

---
+++ src/main/java/com/luo/demo/domain/Person.java
@@ -0,0 +1,53 @@
+package com.luo.demo.domain;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @date:2020-09-25
+ */
+@Component
+@ConfigurationProperties(prefix = "person")
+public class Person {
+    private Integer age;
+    private String name;
+    private Date birthday;
+    private Map<String,String> map;
+    private List<Integer> list;
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public void setMap(Map<String, String> map) {
+        this.map = map;
+    }
+
+    public void setList(List<Integer> list) {
+        this.list = list;
+    }
+
+    @Override
+    public String toString() {
+        return "person{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                ", birthday=" + birthday +
+                ", map=" + map +
+                ", list=" + list +
+                '}';
+    }
+}
--- src/test/java/com/luo/demo/SpringBootDemoApplicationTests.java
+++ src/test/java/com/luo/demo/SpringBootDemoApplicationTests.java
@@ -1,13 +1,31 @@
 package com.luo.demo;
 
+import com.luo.demo.domain.Person;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class SpringBootDemoApplicationTests {
 
+    @Autowired
+    Person person;
+
     @Test
-    void contextLoads() {
+    void contextLoads() throws ClassNotFoundException, NoSuchFieldException, IllegalAccessException, InvocationTargetException {
+
+        System.out.println(person);
+        Class clazz = Class.forName("com.luo.demo.domain.Person");
+        Field name = clazz.getDeclaredField("name");
+        name.setAccessible(true);
+        System.out.println(name.get(person));
     }
 
 }
