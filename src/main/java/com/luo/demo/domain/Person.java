78399a898bb2fa40d726dd16926b71d6a6da1f1a
luoxinyu <luoxinyu@wondersgroup.com>
2020-10-15 09:48:58 +0800

    1

--- src/main/java/com/luo/demo/domain/Person.java
+++ src/main/java/com/luo/demo/domain/Person.java
@@ -19,6 +19,7 @@ public class Person {
     private Date birthday;
     private Map<String,String> map;
     private List<Integer> list;
+    private int a;
 
     public void setAge(Integer age) {
         this.age = age;
